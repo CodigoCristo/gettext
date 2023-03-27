10192e7aa5a4e632fb4a8e2f238af20eb8252a08
Codigo Cristo <cristophero.alvarado@gmail.com>
2023-03-27 04:29:14 -0500

    Add files via upload

---
+++ Bash/script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# script.sh
+
+# INFO : https://www.gnu.org/software/gettext/manual/html_node/index.html
+# INFO : https://www.gnu.org/software/gettext/manual/html_node/Preparing-Shell-Scripts.html
+# INFO DE XGETTEXT : https://www.gnu.org/software/gettext/manual/html_node/xgettext-Invocation.html
+
+
+
+export TEXTDOMAIN=base
+export TEXTDOMAINDIR=locales
+
+. gettext.sh
+
+#echo $(eval_gettext "hi") 
+
+dialog --yesno "$(eval_gettext "hi")" 0 0
